d732e197df14005e2b9fb76040f7df156d1c5db0
NicoleNotFound <zhiqianli618@gmail.com>
2026-01-10 00:16:45 +0100

    fixed data retrieve problem in webscrap_new

--- webscrap_and_data/utils_webscrap.py
+++ webscrap_and_data/utils_webscrap.py
@@ -3,10 +3,78 @@ from pathlib import Path
 import pandas as pd
 from etf_scraper import ETFScraper
 
+def get_ter(isin):
+    url = f"https://www.justetf.com/en/etf-profile.html?isin={isin}"
+
+    rq = requests.get(url, headers=headers)
+    if  rq.status_code == 403:
+        print(isin)
+    html = rq.text
+    soup = BeautifulSoup(html, "html.parser")
+
+    # 找到包含 TER 的 label
+    labels = soup.find_all(
+        "div",
+        class_="val bold",
+        attrs={"data-testid": "etf-profile-header_ter-value"}
+    )
+    ter = None
+    if len(labels) == 1:
+        label = labels[0]
+        ter = label.get_text(strip=True).split()[0]
+        print("ISIN:",isin,"TER:", ter)
+        pattern = r'(\d+\.\d+)(\s*%)'
+        ter= float(re.match(pattern,ter).group(1))
+        return ({'ISIN':isin,"TER":ter})
+    else:
+        print(f'ISIN:{isin} not found')
+        manual_ter_lst.append(isin)
+        return None
+        
+
+        
+def aggregate_etf_data(df, ticker):
+    """
+    input: df, Ticker, TER
+    output: (industry_df, location_df)
+    """
+    # 1. convert: iShares XML 'Gewichtung (%)' e.g. 1.66856% --> /100
+    df['Weight_Decimal'] = pd.to_numeric(df['Gewichtung (%)'], errors='coerce') / 100
+    
+    # 2. industry
+    industry_agg = df.groupby('Sektor')['Weight_Decimal'].sum().reset_index()
+    industry_agg.columns = ['Sector', 'Weight']
+    industry_agg['Ticker'] = ticker
+    
+    # 3. country
+    location_agg = df.groupby('Standort')['Weight_Decimal'].sum().reset_index()
+    location_agg.columns = ['Location', 'Weight']
+    location_agg['Ticker'] = ticker
+    
+    return industry_agg, location_agg
+
+
+def generate_ishares_download_link(product_url, fund_name,ticker):
+    """
+    generate link for downloading holdings info for a specified ETF
+    """
+
+    magic_string = "1535604580385"
+    base_url = f"{product_url}/{magic_string}.ajax"
+    file_name = '-'.join([x.strip().upper() for x in fund_name.split()])
+    params = f"?fileType=xls&fileName={ticker}_fund&dataType=fund"
+    
+    return base_url + params
 
 def get_ticker_holdings(fund_ticker,holdings_date = None, print_output=False):
     """
-    holdings_date: str or None (None to query the latest holdings)"""
+    Retrieves the holdings information for a given ETF ticker using ETFScraper.
+    
+    :param fund_ticker: str
+    :param holdings_date: str or None (None to query the latest holdings)
+    :param print_output: True if you want to print header rows of the holdings info
+    :return: pandas DataFrame containing the holdings information
+    """
     # initialization & setup
     etf_scraper = ETFScraper()
     
@@ -28,6 +96,14 @@ def get_ticker_holdings(fund_ticker,holdings_date = None, print_output=False):
 
 
 def extract_subtable(my_soup, subtable_key):
+    """
+    Extracts a subtable from the provided BeautifulSoup 
+    object(from www.justetf.com) based on the given subtable key (see header_dict).
+    
+    :param my_soup: a BeautifulSoup object containing the HTML content
+    :param subtable_key: a value in header_dict, e.g., "top-holdings", "countries", "sectors"
+    :return: a pandas DataFrame with columns "Name" and "Value" which contains the extracted info
+    """
     header_0 = "etf-holdings"
 
     def extract_name_from_cell(cell):
